537574421ec1886451f81673b5e620c3d5ad1fa7
fran33 <franciscomejias.24@campuscamara.es>
2025-11-04 09:24:44 +0100

    init clase A

---
+++ Grupo11/src/ClaseA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class ClaseA {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
