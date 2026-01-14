005b2da0c7d1cb2709dcdf45ff5315a4579ef86f
Francisco Mejias Gonzalez <franciscomejias.24@campuscamara.es>
2026-01-14 18:44:41 +0100

    Remove unused import for Math in Fecha.java
    Removed unused import statement for Math.

--- Grupo11/src/FicheDAO/Fecha.java
+++ Grupo11/src/FicheDAO/Fecha.java
@@ -3,7 +3,7 @@ package FicheDAO;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Math;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
